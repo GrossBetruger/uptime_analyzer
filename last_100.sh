85ff66ded5e28d82644ad7988d289d4465ba09a9
oren <oren.koriat@OrenKori-LM-IL.local>
2025-11-16 22:40:04 +0200

    feat: color new users

--- last_100.sh
+++ last_100.sh
@@ -1,1 +1,1 @@
-curl "http://34.55.225.231:3000/logs?n=100" | colout "(NisimY)|(TomerB)|(UdiK)" Hash | colout "(online)|(offline)" Hash
+curl "http://34.55.225.231:3000/logs?n=100" | colout "(NisimY)|(TomerB)|(UdiK)|(OferH)|(DaniS)" Hash | colout "(online)|(offline)" Hash
