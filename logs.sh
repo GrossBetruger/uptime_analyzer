b434ae0170d0bbc256b4a0eca05fa745d13af0c5
oren <oren.koriat@OrenKori-LM-IL.local>
2025-11-17 14:35:06 +0200

    feat: color new users

--- logs.sh
+++ logs.sh
@@ -1,3 +1,3 @@
-curl http://34.55.225.231:3000/logs | colout "(NisimY)|(TomerB)|(UdiK)" Hash | colout "(online)|(offline)" Hash  | colout "T(\d{2}:\d{2})" Hash
+curl http://34.55.225.231:3000/logs | colout "(NisimY)|(TomerB)|(UdiK)|(OferH)|(DaniS)" Hash | colout "(online)|(offline)" Hash  | colout "T(\d{2}:\d{2})" Hash
 
 
